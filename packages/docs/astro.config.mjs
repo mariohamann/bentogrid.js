a633e2c9e5e618c3f410b0fb47b728931a04e249
Mario Hamann <mariovhamann@gmail.com>
2023-04-08 23:18:37 +0200

    docs: fix astro build

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://mariohamann.github.io/",
+  base: "/bentogrid.js/"
 });
