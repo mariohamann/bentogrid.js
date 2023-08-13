ca0512466340862095fc7c3d9c40dc58aa819521
Mario Hamann <mario.hamann@virtual-identity.com>
2023-08-13 07:52:54 +0200

    Update astro.config.mjs

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://mariohamann.github.io/",
-  base: "/bentogrid.js/"
+  site: "https://bentogrid.mariohamann.de/"
 });
